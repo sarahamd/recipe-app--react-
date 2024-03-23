465b8b03d7030e68869fc5fbb194bb5c4245dc86
sarahamed <sarahamed2442@gmail.com>
2024-03-23 08:24:22 -0700

    v7

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import style from '../Styles/Landing.module.css';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
 export default function Landing() {
   const [t, i18n] = useTranslation();
-
+  let logedInUser = useSelector((state) => state.logedInUser.logedInUser);
   return (
     <div id={style.landing} dir={`${i18n.language}`==='en'?"ltr":"rtl"}>
       <div className="container-fluid my-4">
@@ -21,11 +22,13 @@ export default function Landing() {
             <p className="my-4 fw-normal " style={{ maxWidth: '590px', fontFamily: 'serif', fontSize: '1.3rem' }}>
             {t('Recipes to Explore, where Passion Meets Palate, and Every Dish is a Symphony of Flavors')}
             </p>
+            {!logedInUser&&
             <button className="btn btn-success shadow-sm px-4">
               <Link to="/signin" className="text-white text-decoration-none">
               {t('Sign up')}
               </Link>
-            </button>
+            </button>}
+            {!logedInUser&&
             <p className="mt-2 fw-normal"  style={{ fontSize: '1.1rem' }}>
             {t('Do you have an account?')}{' '}
               <Link
@@ -34,7 +37,7 @@ export default function Landing() {
               >
                  {t('Log in')}
               </Link>
-            </p>
+            </p>}
           </div>
 
           <div className="col-md-6 text-center">
